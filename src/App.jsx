76611da74f163043dfa0f1bf7887f9d658d789e7
koliiis <ann27052005@gmail.com>
2025-05-19 15:51:30 +0300

    fix1

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function getPreparedGoods(goods, { sortField, isReversed }) {
           return good1.localeCompare(good2);
 
         case SORT_FIELD_LENGTH:
-          return good1[sortField] - good2[sortField];
+          return good1.length - good2.length;
 
         default:
           return 0;
